62cfbd734fe11985dd99d41a6f4e87c6fd8e6a5c
Piotr3009 <piotr.t@skylonconstruction.com>
2026-05-23 13:14:01 +0100

    Update FrontElevation2D.jsx

--- src/components/drawings/FrontElevation2D.jsx
+++ src/components/drawings/FrontElevation2D.jsx
@@ -7,7 +7,7 @@
  */
 import { useMemo } from 'react';
 import { CONSTANTS } from '../../engine/calculations.js';
-import { STROKE, FONT, SIZES, SC_DIVISOR, DimH, DimV, TitleBlock, DIM_OFFSET, DIM_GAP, MARGIN, computeBarPositions } from './drawingUtils.jsx';
+import { STROKE, FONT, SIZES, WEIGHTS, SC_DIVISOR, DimH, DimV, TitleBlock, DIM_OFFSET, DIM_GAP, MARGIN, computeBarPositions } from './drawingUtils.jsx';
 
 export default function FrontElevation2D({ windowSpec, derived }) {
   const drawing = useMemo(() => {
